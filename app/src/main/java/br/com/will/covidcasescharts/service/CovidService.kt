11fb78462688783e2b97df2cf60bd99740886167
wilkison martins da silva <wilkisonmartinsdasilva@gmail.com>
2022-01-28 19:08:48 -0400

    feat: fix dependency of GsonConverterFactory and implement to ger nationalData of API

--- app/src/main/java/br/com/will/covidcasescharts/service/CovidService.kt
+++ app/src/main/java/br/com/will/covidcasescharts/service/CovidService.kt
@@ -5,7 +5,7 @@ import retrofit2.Call
 import retrofit2.http.GET
 
 interface CovidService {
-    
+
     @GET("us/daily.json")
     fun getNationalData(): Call<List<CovidData>>
 
--- app/src/main/java/br/com/will/covidcasescharts/ui/MainActivity.kt
+++ app/src/main/java/br/com/will/covidcasescharts/ui/MainActivity.kt
@@ -2,10 +2,49 @@ package br.com.will.covidcasescharts.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import br.com.will.covidcasescharts.model.CovidData
+import br.com.will.covidcasescharts.service.CovidService
+import com.google.gson.GsonBuilder
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 
 class MainActivity : AppCompatActivity() {
+
+    private val BASE_URL = "https://api.covidtracking.com/v1/"
+    private val TAG = "MainActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val gson = GsonBuilder().create()
+        val retrofit = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .build()
+
+        val covidService = retrofit.create(CovidService::class.java)
+
+        //Fetch the national data
+        covidService.getNationalData().enqueue(object: Callback<List<CovidData>>{
+            override fun onResponse(
+                call: Call<List<CovidData>>,
+                response: Response<List<CovidData>>
+            ) {
+
+            }
+
+            override fun onFailure(call: Call<List<CovidData>>, t: Throwable) {
+                Log.e(TAG, "onFailure ${t}")
+            }
+
+        })
+        //Fetch the state data
+
     }
 }
